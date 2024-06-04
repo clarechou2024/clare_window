655c194a160074451716d83a0507b77e5bb78203
clarechou <clare40318@gmail.com>
2024-06-04 11:54:41 +0800

    0604

---
+++ 2024_06_04/index.py
@@ -0,0 +1,16 @@
+#只有主執行檔,tools是function,裡面放class
+
+from pprint import pprint
+import tools
+
+def main():
+    try:
+        all_data:dict[any] = tools.download_json()
+    except Exception as error:
+        print(error)
+    else:
+       data:list[dict] = tools.get_data(all_data)
+       pprint(data)
+
+if __name__=='__main__':
+    main()
