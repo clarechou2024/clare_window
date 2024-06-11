901ad79c3b6f597dcb91f43845a52deb5620d7ef
clarechou2024 <clare40318@gmail.com>
2024-06-11 15:47:38 +0800

    modify

---
+++ 2024_06_11_01/index.py
@@ -0,0 +1,112 @@
+##物件導向做法
+
+import tkinter as tk
+from tkinter import ttk
+from ttkthemes import ThemedTk ##2
+from tkinter import messagebox
+from tools import CustomMessagebox
+
+
+class Window(ThemedTk):       ##2--建立Window視窗
+    def __init__(self,theme:str|None,**kwargs):    #**kwargs:沒有限定名稱的呼叫
+        super().__init__(**kwargs)
+        self.title("BMI計算機")
+        #self.configure(bg='#D3D3D3')  ##背景色
+        #self.geometry("350x350+100+50")   #不建議設大小
+        self.resizable(False,False)
+        style = ttk.Style()    #內部提供的樣式
+        #print(style.theme_name)
+        style.configure('input.TFrame',background="#ffffff")
+        style.configure('press.TButton',font=('Arial',16))
+        #========================================================
+
+        titleFrame = ttk.Frame(self)
+        title_label = ttk.Label(self, text="BMI計算器", font=("Arial", 20))
+        title_label.pack(pady=10)
+        titleFrame.pack(padx=100,pady=(0,20))  #把容器撐開
+        #========================================================
+
+        input_frame = ttk.Frame(self,style ='input.TFrame')
+        
+        # 姓名
+
+        label_name = ttk.Label(input_frame, text="姓名:")
+        label_name.grid(row=0, column=0, padx=5, pady=5,sticky=tk.E)
+
+        self.name_value = tk.StringVar()
+        self.name_value.set('')
+        entry_name = ttk.Entry(input_frame,textvariable=self.name_value)
+        entry_name.grid(row=0, column=1, padx=5, pady=5)
+
+        # 身高體重
+        label_height = ttk.Label(input_frame, text="身高 (cm):")
+        label_height.grid(row=1, column=0, padx=5, pady=5,sticky=tk.E)  #sticky:靠哪邊
+        self.height_value = tk.StringVar()
+        self.height_value.set('')
+        entry_height = ttk.Entry(input_frame,textvariable=self.height_value)
+        entry_height.grid(row=1, column=1, padx=5, pady=5)
+
+        label_weight = ttk.Label(input_frame, text="體重 (kg):")
+        label_weight.grid(row=2, column=0, padx=5, pady=5,sticky=tk.E)
+
+        self.weight_value = tk.StringVar()
+        self.weight_value.set('')
+        entry_weight = ttk.Entry(input_frame,textvariable=self.weight_value)
+        entry_weight.grid(row=2, column=1, padx=5, pady=5)
+
+        input_frame.pack(pady=10,padx=30)
+        #==========================================================
+
+        button_calculate = ttk.Button(self, text="計算", command=self.show_bmi_result,style='press.TButton')
+        button_calculate.pack(side=tk.RIGHT,padx=(0,35),pady=10,ipadx=5,ipady=5)
+
+
+    def show_bmi_result(self):
+        try:
+            name:str = self.name_value.get()
+            height:int = int(self.height_value.get())
+            weight:int = int(self.weight_value.get())
+
+        #except UnboundLocalError:
+            #messagebox.showwarning("Warning","欄位未填寫")
+        except ValueError:
+            messagebox.showwarning("Warning","格式錯誤,欄位未填寫")
+        except Exception:
+            messagebox.showwarning("Warning","不知明的錯誤")
+        else:
+            self.show_result(name=name,height=height,weight=weight)
+
+        #print(name,height,weight)
+
+    
+    def show_result(self,name:str,height:int,weight:int):
+            bmi = weight / (height / 100) ** 2
+            if bmi < 18.5:
+                status = "過輕"
+                ideal_weight = 18.5 * (height / 100) ** 2
+                weight_change = ideal_weight - weight
+                status_color = "red"
+                advice = f"需要再增加 {abs(weight_change):.2f} 公斤才能達到正常體重。"
+            elif 18.5 <= bmi <= 24.9:
+                status = "正常"
+                status_color = "blue"
+                advice = "體重正常，請保持！"
+            else:
+                status = "過重"
+                ideal_weight = 24.9 * (height / 100) ** 2
+                weight_change = weight - ideal_weight
+                status_color = "red"
+                advice = f"需要再減少 {abs(weight_change):.2f} 公斤才能達到正常體重。"
+            
+            CustomMessagebox(self,title="BMI",name=name,bmi=bmi,status=status,advice=advice,status_color=status_color)
+    def __repr__(self):
+        return "我是window的實體"
+
+
+def main():                 ##1--建立main()
+    window = Window(theme='arc')
+    window.mainloop()
+    
+
+if __name__ == '__main__':
+    main()
