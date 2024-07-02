639613941c5c504beaa9617d07311d4cdff55c49
clarechou2024 <clare40318@gmail.com>
2024-07-02 20:16:55 +0800

    modify

--- insert_youbike_data/cron.py
+++ insert_youbike_data/cron.py
@@ -1,5 +1,8 @@
 from crontab import CronTab
+import os
 cron = CronTab(user=True)
-job = cron.new(command="echo 'hello world'")
-job.minute.every(1)
+path = os.path.abspath("./lesson2.py")
+job = cron.new(command=f"/home/pi/miniconda3/bin/python'{path}'")
+job.minute.every(10)
+#job.set_comment("Output hello world")
 cron.write()
