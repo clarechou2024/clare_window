dbcd3af286692f965e307d55f8cbea1973ad292f
clarechou <clare40318@gmail.com>
2024-07-09 16:46:38 +0800

    modify

--- 2024_07_09/main1.py
+++ 2024_07_09/main1.py
@@ -16,6 +16,5 @@ def profile(username):
 with app.test_request_context():
     print(url_for('index'))
     print(url_for('login'))
-    print(url_for('login', name='Ann',password='12345'))
+    print(url_for('login', name='Ann', password='1234'))
     print(url_for('profile', username='John Doe'))
-   
