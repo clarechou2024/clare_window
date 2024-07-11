373ccc9921d5e71989b9753dd2019d8fd70658b2
clarechou <clare40318@gmail.com>
2024-07-11 16:37:58 +0800

    modify

--- 2024_07_11/main.py
+++ 2024_07_11/main.py
@@ -8,9 +8,9 @@ def index():
     selected_area = request.args.get('area')
     areas =[tup[0]for tup in data.get_areas()]
 
+#if的簡化寫法:
     selected_area = '士林區' if selected_area is None else selected_area
     return render_template("index.html.jinja",areas= areas,show_area=selected_area)
-
 
     # if selected_area is None:
     #     return render_template("index.html.jinja",areas= areas,show_area='士林區')
