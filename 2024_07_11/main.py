ce4cb3512afa35088bc5f9bfb5f4f62999e205ed
clarechou <clare40318@gmail.com>
2024-07-15 11:16:29 +0800

    modify

--- 2024_07_11/main.py
+++ 2024_07_11/main.py
@@ -6,13 +6,26 @@ app = Flask(__name__)
 def index():
     #print(list(map(lambda value:value[0],data.get_areas())))
     selected_area = request.args.get('area')
-    areas =[tup[0]for tup in data.get_areas()]
-
-#if的簡化寫法:
+    areas = [tup[0] for tup in data.get_areas()]
     selected_area = '士林區' if selected_area is None else selected_area
-    return render_template("index.html.jinja",areas= areas,show_area=selected_area)
+    detail_snaes = data.get_snaOfArea(area=selected_area)
 
-    # if selected_area is None:
-    #     return render_template("index.html.jinja",areas= areas,show_area='士林區')
-    # else:
-    #     return render_template("index.html.jinja",areas= areas,show_area=selected_area)
+    return render_template('index.html.jinja',areas=areas,show_area=selected_area,detail_snaes=detail_snaes)   
+
+#     #print(list(map(lambda value:value[0],data.get_areas())))
+#     selected_area = request.args.get('area')
+#     areas =[tup[0]for tup in data.get_areas()]
+
+# #if的簡化寫法:
+#     selected_area = '士林區' if selected_area is None else selected_area
+#     detail_snaes = data.get_snaOfArea(area = selected_area)
+
+#     #areas->所有行政區
+#     #show_area->要顯示的行政區
+#     #detail_snaes->該行政區所有站點資訊
+#     return render_template('index.html.jinja',areas=areas,show_area=selected_area,detail_snaes=detail_snaes)
+
+#     # if selected_area is None:
+#     #     return render_template("index.html.jinja",areas= areas,show_area='士林區')
+#     # else:
+#     #     return render_template("index.html.jinja",areas= areas,show_area=selected_area)
