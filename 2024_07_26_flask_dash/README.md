893409ad4145fa6b2cfd9316adf3157ecdccfc4c
clarechou <clare40318@gmail.com>
2024-07-26 10:05:28 +0800

    modify

---
+++ 2024_07_26_flask_dash/README.md
@@ -0,0 +1,36 @@
+## 網頁製作
+
+### 課程參考網址如下:
+
+
+### 1. Jinja2
+
+[Jinja2參考網址](https://jinja.palletsprojects.com/en/3.1.x/templates/)
+
+
+### 2. Flask
+
+[Flask參考網址](https://flask.palletsprojects.com/en/3.0.x/)
+
+------
+
+#### 執行指令
+![alt text](image.png)
+
+------
+
+## 完成網頁網址
+[網址連結](https://clare-window.onrender.com/)
+
+
+## pgAdmin連線 (帳號/密碼/網址)
+--postgresql://--
+
+--tvdi_****_user:--
+
+--MVXOacNc7LvzZSZ****4LKO6s9ZhGsto@--
+
+--dpgcpscsal6l47c73e3****-a.singapore-postgres.render.com/--
+
+--tvdi_****--
+
---
+++ 2024_07_26_flask_dash/main.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template,request
+import data
+
+app = Flask(__name__)
+@app.route("/")   #/-->首頁
+def index1():
+    return render_template("index1.html.jinja")
+
+@app.route("/index1")
+def index():
+    #print(list(map(lambda value:value[0],data.get_areas())))
+    selected_area = request.args.get('area')
+    areas =[tup[0] for tup in data.get_areas()]
+
+#if的簡化寫法:
+    selected_area = '士林區' if selected_area is None else selected_area
+    detail_snaes = data.get_snaOfArea(area = selected_area)
+
+    #areas->所有行政區
+    #show_area->要顯示的行政區
+    #detail_snaes->該行政區所有站點資訊
+    return render_template('index.html.jinja',areas=areas,show_area=selected_area,detail_snaes=detail_snaes)
+
+    # if selected_area is None:
+    #     return render_template("index.html.jinja",areas= areas,show_area='士林區')
+    # else:
+    #     return render_template("index.html.jinja",areas= areas,show_area=selected_area)
