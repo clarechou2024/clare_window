fa06abc183dd7ad5e6554001cc97665f8f469e71
clarechou <clare40318@gmail.com>
2024-07-30 14:58:09 +0800

    modify

--- 2024_07_30_flask_dash/auth/main.py
+++ 2024_07_30_flask_dash/auth/main.py
@@ -1,10 +1,20 @@
 from flask import Blueprint,render_template
-# from flask_wtf import FlaskForm
+from flask_wtf import FlaskForm
+from wtforms import EmailField,PasswordField
+from wtforms.validators import DataRequired,Length
 
 auth_blueprint = Blueprint('auth',__name__)
 # #設加密字串
 # WTF_CSRF_SECRET_KEY = 'a random string'
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('郵件信箱',validators=[DataRequired()])
+    password = PasswordField('密碼',validators=[DataRequired(),Length(min=4,max=20)])
+
+
 @auth_blueprint.route("/auth/",methods=['GET','POST'])
 @auth_blueprint.route("/auth/login",methods=['GET','POST'])
 def index():
-    return render_template('/auth/login.html.jinja')
+    form = LoginForm()
+    return render_template('/auth/login.html.jinja',form=form)
